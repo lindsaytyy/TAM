3c3b5dc91a79ed33bdc02eb1ec6e67c46ad641b3
谭月月 <ffos.tanyy@chinatelecom.cn>
2025-03-10 21:33:01 +0800

    Configure CI/CD pipeline with dotenv-vault

--- TAM_FRONTEND/next.config.js
+++ TAM_FRONTEND/next.config.js
@@ -2,6 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker", "rc-input"],
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
